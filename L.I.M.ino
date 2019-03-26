b7f65420f6653518841049be8d3d494318ca3a15
Edou68FR <31824624+Edou68FR@users.noreply.github.com>
2019-03-27 00:01:25 +0100

    :fire: New big update
    - Fixed a lot of things
    - Added notification mode
    - Added multicolor mode

--- L.I.M.ino
+++ L.I.M.ino
@@ -1,7 +1,7 @@
-// Output
-int redPin = 3;   // Red LED,   connected to digital pin 3
-int grnPin = 5;  // Green LED, connected to digital pin 5
-int bluPin = 6;  // Blue LED,  connected to digital pin 6
+ut// Output
+int redPin = 10;   // Red LED,   connected to digital pin 3
+int grnPin = 9;  // Green LED, connected to digital pin 5
+int bluPin = 11;  // Blue LED,  connected to digital pin 6
 int photor = A1; // Photo résistance, connected to analog pin A1
 
 // Color arrays
@@ -11,11 +11,10 @@ int white[3]  = { 100, 100, 100 };
 int red[3]    = { 100, 0, 0 };
 int green[3]  = { 0, 100, 0 };
 int blue[3]   = { 0, 0, 100 };
-int yellow[3] = { 40, 95, 0 };
+int lblue[3]   = { 0, 80, 100 };
+int yellow[3] = { 100, 100, 0 };
 int dimWhite[3] = { 30, 30, 30 };
-// etc.
 
-// Set initial color
 int redVal = black[0];
 int grnVal = black[1]; 
 int bluVal = black[2];
@@ -24,11 +23,41 @@ int wait = 100;      // 10µs internal crossFade delay; increase for slower fade
 int hold = 1;       // Optional hold when a color is complete, before the next crossFade
 int repeat = 0;     // How many times should we loop before stopping? (0 for no stop)
 int j = 0;          // Loop counter for repeat
-int mode = 1;       // actual mode ( 1 = LightMode, 2 = animation, 3 = audio, 4 = manualMode ).
+
+
+int mode = 2;       // actual mode ( 1 = LightMode, 2 = animation, 3 = audio, 4 = Snap, 5  ).
 // Initialize color variables
 int prevR = redVal;
 int prevG = grnVal;
 int prevB = bluVal;
+
+void serialEvent() 
+{
+  char r;
+  while(Serial.available()) 
+  {
+    r = (char)Serial.read();
+    Serial.println(millis());
+    Serial.println(r);
+     switch(r){
+      case 's':
+        mode = 4;
+      break;
+      case 'f':
+        mode = 5;
+      break;
+      case 't':
+        mode = 6;
+      break;
+      case 'm':
+        mode = 7;
+      break;
+    }
+    Serial.end();  
+    Serial.begin(9600);
+  }
+}
+
 
 // Set up the LED outputs
 void setup()
@@ -43,73 +72,64 @@ void setup()
 // Main program: list the order of crossfades
 void loop()
 {
-  int bmode = digitalRead(buttonmode);
-  if(bmode == HIGH)
-  {
-    while( bmode == HIGH){}
-    mode = mode++;
-  }
-}
-
-void serialEvent(){
-    while (Serial.available()) {
-    char var = (char)Serial.read();
-    inputString += var;
-    if (var == '\n') {
-      stringComplete = true;
-    }
-  }
-}
-
-void notif()
-{
-  switch (app) {
-    case 1:
-      for(int k; k >= 10; k++)
+  switch (mode) {
+    wait = 100;
+    case 2:
+      wait = 1000;
+      crossFade(blue);
+      crossFade(lblue);
+      crossFade(green);
+      crossFade(yellow);
+      crossFade(red);
+      crossFade(white);
+    break;
+    case 4:
+      for(int k; k <= 2; k++)
       {
         crossFade(yellow);
         delay(900);
-        crossFade(white);
-         delay(900);
-      }
-    break;
-    case 2:
-      for(int k; k >= 10; k++)
-      {
-        crossFade(blue);
-        delay(900);
-        crossFade(white);
-        delay(900);
-      }
-    break;
-    case 3:
-      for(int k; k >= 10; k++)
-      {
-        crossFade(lblue);
-        delay(900);
-        crossFade(white);
-        delay(900);
-      }
-    break;
-    case 4:
-      for(int k; k >= 10; k++)
-      {
-        crossFade(green);
-        delay(900);
-        crossFade(white);
+        crossFade(black);
         delay(900);
       }
     break;
     case 5:
-      for(int k; k >= 10; k++)
+      for(int k; k <= 2; k++)
       {
-        crossFade(insta);
+        crossFade(blue);
+        delay(900);
+        crossFade(black);
+        delay(900);
+      }
+    break;
+    case 6:
+      for(int k; k <= 2; k++)
+      {
+        crossFade(lblue);
+        delay(900);
+        crossFade(black);
+        delay(900);
+      }
+    break;
+    case 7:
+      for(int k; k <= 2; k++)
+      {
+        crossFade(green);
+        delay(900);
+        crossFade(black);
+        delay(900);
+      }
+    break;
+    case 8:
+      for(int k; k <= 2; k++)
+      {
+      //  crossFade(insta);
         delay(900);
         crossFade(white);
         delay(900);
       }
     break;
   }
+  mode = 2;
 }
 
 void LightMode() 
@@ -181,7 +201,7 @@ void crossFade(int color[3]) {
   int stepG = calculateStep(prevG, G); 
   int stepB = calculateStep(prevB, B);
 
-  for (int i = 0; i <= 1020; i++) {
+  for (int i = 0; i <= 1020 && !Serial.available(); i++) {
     redVal = calculateVal(stepR, redVal, i);
     grnVal = calculateVal(stepG, grnVal, i);
     bluVal = calculateVal(stepB, bluVal, i);
@@ -189,7 +209,7 @@ void crossFade(int color[3]) {
     analogWrite(redPin, redVal);   // Write current values to LED pins
     analogWrite(grnPin, grnVal);      
     analogWrite(bluPin, bluVal); 
-
+    
     delayMicroseconds(wait); // Pause for 'wait' milliseconds before resuming the loop
   }
   // Update current values for next loop
@@ -198,5 +218,7 @@ void crossFade(int color[3]) {
   prevB = bluVal;
   delay(hold); // Pause for optional 'wait' milliseconds before resuming the loop
 }
+
+
 
 
